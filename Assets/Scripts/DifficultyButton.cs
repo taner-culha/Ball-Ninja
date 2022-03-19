35b6ff45026f5a057e87e222491bb816ea56cce1
taner-culha <tanerculha@outlook.com.tr>
2022-03-19 21:58:30 +0300

    Ball Ninja
    Ball Ninja

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    public int difficulty;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+    void SetDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
